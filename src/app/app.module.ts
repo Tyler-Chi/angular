b0ebb2ef5892d1d0dd6c077b58ec2d4c5be84d92
Tyler Chi <itychi@gmail.com>
2018-04-21 01:43:32 -0700

    done for the night

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
+//don't need to add the ts to this.
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
-
+//any component used in this app needs to be included in the declarations.
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
